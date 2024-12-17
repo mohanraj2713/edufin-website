68d63adb7d9acd7c8c044d58556cbb4c5cfa3773
Mohanraj Selvaraj <mohanraj.selvaraj@edufin.in>
2024-12-17 22:58:28 +0530

    testing

--- src/app/terms-and-conditions/page.tsx
+++
@@ -1,9 +0,0 @@
-import TermsAndConditions from "@/components/Pages/TermsComponents/TermsAndConditions";
-const Terms = () => {
-  return (
-    <>
-      <TermsAndConditions />
-    </>
-  );
-};
-export default Terms;
